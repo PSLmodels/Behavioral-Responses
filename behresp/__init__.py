7cdd7f5ce86d6a595306fc8a622c184cfe4d7782
Martin Holmer <martin.holmer@gmail.com>
2018-10-19 22:05:30 -0400

    Temporarily remove behresp.tbi import in __init__.py

--- behresp/__init__.py
+++ behresp/__init__.py
@@ -2,7 +2,7 @@
 Specify what is available to import from the behresp package.
 """
 from behresp.behavior import PARAM_INFO, response
-from behresp.tbi import run_nth_year_behresp_model
+# TODO: from behresp.tbi import run_nth_year_behresp_model
 
 from behresp._version import get_versions
 __version__ = get_versions()['version']
