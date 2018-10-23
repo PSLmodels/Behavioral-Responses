74acdf3c917322ea6ed55fbc929c664339b469d2
Martin Holmer <martin.holmer@gmail.com>
2018-10-23 07:43:36 -0400

    Strengthen response tests

--- behresp/tests/test_behavior.py
+++ behresp/tests/test_behavior.py
@@ -34,7 +34,11 @@ def test_response_function():
     """
     # pylint: disable=too-many-locals
 
-    behy_json = '{"_BE_sub": {"2018": [0.25]}}'
+    behy_json = """{
+    "_BE_sub": {"2018": [0.25]},
+    "_BE_inc": {"2018": [-0.1]},
+    "_BE_cg": {"2018": [-0.79]}
+    }"""
     behy_dict = tc.Calculator.read_json_assumptions(behy_json)
     behy_obj = tc.Behavior()
     behy_obj.update_behavior(behy_dict)
@@ -48,11 +52,16 @@ def test_response_function():
     assert not pol.parameter_errors
     calc2x = tc.Calculator(records=rec, policy=pol)
     calc2y = tc.Calculator(records=rec, policy=pol, behavior=behy_obj)
+    calc2s = tc.Calculator(records=rec, policy=pol)
 
     calc1x.advance_to_year(refyear)
     calc2x.advance_to_year(refyear)
     calc1y.advance_to_year(refyear)
     calc2y.advance_to_year(refyear)
+    calc2s.advance_to_year(refyear)
+
+    calc2s.calc_all()
+    itax2s = round(calc2s.weighted_total('iitax') * 1e-9, 3)
 
     behx_json = """{
     "BE_sub": {"2018": 0.25},
@@ -63,19 +72,18 @@ def test_response_function():
     df1, df2 = response(calc1x, calc2x, behx_dict, trace=True)
     itax1x = round((df1['iitax'] * df1['s006']).sum() * 1e-9, 3)
     itax2x = round((df2['iitax'] * df2['s006']).sum() * 1e-9, 3)
-    print(itax1x, itax2x)
+    assert np.allclose([itax1x, itax2x], [1413.428, 1359.683])
 
     calc2y_behv = tc.Behavior.response(calc1y, calc2y, trace=True)
     itax1y = round(calc1y.weighted_total('iitax') * 1e-9, 3)
     itax2y = round(calc2y_behv.weighted_total('iitax') * 1e-9, 3)
-    print(itax1y, itax2y)
-
-    assert np.allclose([itax1x, itax2x], [1413.428, 1359.683])
+    assert np.allclose([itax1x, itax2x], [itax1y, itax2y])
 
     beh_dict = tc.Calculator.read_json_assumptions('{}')
     df1, df2 = response(calc1x, calc2x, beh_dict)
-    assert isinstance(df1, pd.DataFrame)
-    assert isinstance(df2, pd.DataFrame)
+    itax1n = round((df1['iitax'] * df1['s006']).sum() * 1e-9, 3)
+    itax2n = round((df2['iitax'] * df2['s006']).sum() * 1e-9, 3)
+    assert np.allclose([itax1n, itax2n], [itax1x, itax2s])
 
     beh_dict = tc.Calculator.read_json_assumptions(
         '{"BE_inc": {"2018": -0.1}}'
