4931dced305061e0fd3033d2fe93c84ffe6c4997
matthjensen <matthewjensen@Matthews-MBP.fios-router.home>
2019-02-14 07:44:46 -0500

    strengthen test sub_effect_independence

--- behresp/tests/test_behavior.py
+++ behresp/tests/test_behavior.py
@@ -150,7 +150,8 @@ def test_sub_effect_independence():
     input_csv = (u'RECID,MARS,e00200,e00200p,p23250\n'
                  u'1,1,1000000,1000000,0     \n'
                  u'2,1,1000000,1000000,500000\n'
-                 u'3,1,1000000,1000000,-50000\n')
+                 u'3,1,1000000,1000000,-50000\n'
+                 u'4,3,1000000,1000000,-50000\n')
     recs = tc.Records(data=pd.read_csv(StringIO(input_csv)),
                       start_year=refyear,
                       gfactors=None, weights=None)
@@ -169,15 +170,19 @@ def test_sub_effect_independence():
     chg_funit1 = df2['c04800'][0] - df1['c04800'][0]  # funit with RECID=1
     chg_funit2 = df2['c04800'][1] - df1['c04800'][1]  # funit with RECID=2
     chg_funit3 = df2['c04800'][2] - df1['c04800'][2]  # funit with RECID=3
+    chg_funit4 = df2['c04800'][3] - df1['c04800'][3]  # funit with RECID=3
     del df1
     del df2
     # confirm reform reduces taxable income when assuming substitution effect
     assert chg_funit1 < 0
     assert chg_funit2 < 0
     assert chg_funit3 < 0
+    assert chg_funit4 < 0
     # confirm change in taxable income is same for all three filing units
     assert np.allclose(chg_funit2, chg_funit1)
     assert np.allclose(chg_funit3, chg_funit1)
+    assert np.allclose(chg_funit4, chg_funit1)
     del chg_funit1
     del chg_funit2
     del chg_funit3
+    del chg_funit4
